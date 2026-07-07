827cfabbed5842cfd0ad6bdbe400b6d8bd6f938e
shubhamyx <sy9833414680@gmail.com>
2026-07-07 17:55:17 +0530

    Add Position, PriceTick, AuditLog entities

---
+++ src/TradingPlatform.Domain/Entities/Postion.cs
@@ -0,0 +1,45 @@
+﻿namespace TradingPlatform.Domain.Entities;
+
+public class Position
+{
+    public Guid Id { get; private set; }
+    public Guid UserId { get; private set; }
+    public Guid InstrumentId { get; private set; }
+    public decimal Quantity { get; private set; }
+    public decimal AvgEntryPrice { get; private set; }
+
+    private Position() { } // for EF Core
+
+    public Position(Guid userId, Guid instrumentId)
+    {
+        Id = Guid.NewGuid();
+        UserId = userId;
+        InstrumentId = instrumentId;
+        Quantity = 0;
+        AvgEntryPrice = 0;
+    }
+
+    public void Increase(decimal fillQuantity, decimal fillPrice)
+    {
+        if (fillQuantity <= 0)
+            throw new ArgumentException("Fill quantity must be greater than zero.", nameof(fillQuantity));
+
+        decimal totalCost = (Quantity * AvgEntryPrice) + (fillQuantity * fillPrice);
+        Quantity += fillQuantity;
+        AvgEntryPrice = totalCost / Quantity;
+    }
+
+    public void Decrease(decimal fillQuantity)
+    {
+        if (fillQuantity <= 0)
+            throw new ArgumentException("Fill quantity must be greater than zero.", nameof(fillQuantity));
+
+        if (fillQuantity > Quantity)
+            throw new InvalidOperationException("Cannot reduce position below zero.");
+
+        Quantity -= fillQuantity;
+
+        if (Quantity == 0)
+            AvgEntryPrice = 0;
+    }
+}
